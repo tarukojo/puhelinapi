6cae8e5f8adcc9f66c0199a60eab593c746d8f22
Taru Kojo <tarukojo@gmail.com>
2018-02-04 08:44:36 +0200

    Osa 3 tehtava 3.4 puhelinluettelon backend osa4

--- index.js
+++ index.js
@@ -48,6 +48,13 @@ let persons =  [
     }
   })
 
+  app.delete('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    person = persons.filter(person => person.id !== id)
+  
+    response.status(204).end()
+  })
+
   const PORT = 3001
   app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
