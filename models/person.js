21bfd18d6e9300c2478d263be1f908f65e5ef760
Taru Kojo <tarukojo@gmail.com>
2018-02-10 07:32:21 +0200

    Herokuun vienti

--- models/person.js
+++ models/person.js
@@ -1,6 +1,10 @@
 const mongoose = require('mongoose')
 
-const url = 'mongodb://fullstack:sekred@ds123258.mlab.com:23258/persons'
+if ( process.env.NODE_ENV !== 'production' ) {
+  require('dotenv').config()
+}
+
+const url = process.env.MONGODB_URI
 
 mongoose.connect(url)
 mongoose.Promise = global.Promise
